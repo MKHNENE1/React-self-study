8a1596e8f2b9e0b38aa9ff8f5e3775def0a0ea28
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2024-12-17 18:36:18 +0200

    update logging switcher

--- src/components/NavBarLink.jsx
+++ src/components/NavBarLink.jsx
@@ -1,12 +1,14 @@
+/* eslint-disable react/prop-types */
 import { NavLink } from "react-router";
 
-export default function NavBarLink({ to, classes, label }) {
+export default function NavBarLink({ to, classes, label, click }) {
   return (
     <NavLink
       className={({ isActive }) =>
         isActive ? `underline ${classes}` : `no-underline ${classes}`
       }
       to={to}
+      onClick={click}
     >
       {label}
     </NavLink>
