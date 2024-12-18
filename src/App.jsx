bc8350ee88b671afa67bde2f17acaf809fe9bdbc
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2024-12-18 02:42:01 +0200

    update cart and form handeling

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,21 @@ import NotFound from "./pages/NotFound";
 import Nested1 from "./components/nested/Nested1";
 import Nested2 from "./components/nested/Nested2";
 import Nested3 from "./components/nested/Nested3";
+import Menu from "./pages/Menu";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   const [items, setItems] = useState([
-    { name: "frise", count: 0 },
-    { name: "burger", count: 0 },
-    { name: "water", count: 0 },
+    { id: 1, name: "Big frise", count: 0, isInCart: false, price: 30 },
+    { id: 2, name: "Big burger", count: 0, isInCart: false, price: 50 },
+    { id: 3, name: "Big water", count: 0, isInCart: false, price: 10 },
+    { id: 4, name: "Medium frise", count: 0, isInCart: false, price: 20 },
+    { id: 5, name: "Medium burger", count: 0, isInCart: false, price: 40 },
+    { id: 6, name: "Medium water", count: 0, isInCart: false, price: 7 },
+    { id: 7, name: "Small frise", count: 0, isInCart: false, price: 10 },
+    { id: 8, name: "Small burger", count: 0, isInCart: false, price: 30 },
+    { id: 9, name: "Small water", count: 0, isInCart: false, price: 5 },
   ]);
 
   function handelClick(num, item) {
@@ -36,13 +45,21 @@ function App() {
     const itemIndex = newItems.indexOf(item);
     setItems(newItems.filter((item, index) => index != itemIndex));
   }
+  function handelAddToCart(id) {
+    // setItems({...items, items.filter((item)=> item.id == id)})
+    const newItems = [...items];
+    const findItem = newItems.findIndex((item) => item.id == id);
+    newItems[findItem] = { ...newItems[findItem] };
+    newItems[findItem].isInCart = !newItems[findItem].isInCart;
+    newItems[findItem].count = 1;
+    setItems(newItems);
+  }
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+  // console.log(items);
   return (
     <BrowserRouter>
       <Navbar
-        noOfItems={items.filter((item) => item.count > 0).length}
+        noOfItems={items.filter((item) => item.isInCart).length}
         isLoggedIn={isLoggedIn}
         setIsLoggedIn={setIsLoggedIn}
       />
@@ -50,13 +67,17 @@ function App() {
         <Routes>
           <Route
             path="/"
+            element={<Menu items={items} handelAddToCart={handelAddToCart} />}
+          />
+          <Route
+            path="/cart"
             element={
               <Cart
                 isLoggedIn={isLoggedIn}
-                items={items}
+                items={items.filter((item) => item.isInCart)}
                 handelClick={handelClick}
                 handelReset={handelReset}
-                handelDelete={handelDelete}
+                handelDelete={handelAddToCart}
               />
             }
           />
