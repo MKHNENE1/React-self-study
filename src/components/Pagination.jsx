298ce09cadca968d7144a61916a3b9a6286a9218
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-26 10:08:14 +0200

    update pagination component

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+/* eslint-disable react/prop-types */
+import { getArrayFromNumber } from "../utils/getArrayFromNumber";
 
-export default function Pagination() {
-  return <div>Pagination</div>;
+export default function Pagination({
+  noOfPages,
+  currentPage,
+  handleChangeCurrentPage,
+}) {
+  const pages = getArrayFromNumber(noOfPages);
+  return (
+    <div className={`flex justify-center gap-2 my-10`}>
+      {pages.map((page, index) => (
+        <div
+          onClick={() => handleChangeCurrentPage(page)}
+          key={index}
+          className={`w-8 h-w-8 border-2 flex justify-center items-center duration-300 cursor-pointer ${
+            currentPage === page && "bg-slate-300"
+          }`}
+        >
+          {page}
+        </div>
+      ))}
+    </div>
+  );
 }
