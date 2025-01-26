7829d038f58c676bb1b4da2fc12b05c39b681503
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-01-26 10:07:11 +0200

    update filter component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,5 +1,24 @@
-import React from "react";
-
-export default function Filter() {
-  return <div>Filter</div>;
+/* eslint-disable react/prop-types */
+export default function Filter({
+  categories,
+  selectedCategory,
+  handleSelectCategory,
+}) {
+  return (
+    <div className="relative w-full">
+      <div className="sticky top-9 w-96 text-lg">
+        {categories.map((category, index) => (
+          <div
+            onClick={() => handleSelectCategory(category.id)}
+            className={`w-full duration-300 border border-t-0 cursor-pointer ${
+              index === 0 && " border-t"
+            } p-1 ${selectedCategory === category.id && "bg-slate-300"}`}
+            key={index}
+          >
+            {category.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
