0c2602083c25a872e0c027461786e86e6148c65c
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-02-04 20:05:00 +0200

    update filter component

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -3,13 +3,20 @@ export default function Filter({
   categories,
   selectedCategory,
   handleSelectCategory,
+  setLoading,
 }) {
   return (
     <div className="relative w-full">
       <div className="sticky top-9 w-96 text-lg">
         {categories.map((category, index) => (
           <div
-            onClick={() => handleSelectCategory(category.id)}
+            onClick={() => {
+              setLoading(true);
+              handleSelectCategory(category.id);
+              setTimeout(() => {
+                setLoading(false);
+              }, 2000);
+            }}
             className={`w-full duration-300 border border-t-0 cursor-pointer ${
               index === 0 && " border-t"
             } p-1 ${selectedCategory === category.id && "bg-slate-300"}`}
