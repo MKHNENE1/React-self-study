9f1122a5bc471612db66b213977f6b65753e903e
Mohamed Halabia <102330949+MKHNENE1@users.noreply.github.com>
2025-02-04 20:04:28 +0200

    update pagination component

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -5,13 +5,20 @@ export default function Pagination({
   noOfPages,
   currentPage,
   handleChangeCurrentPage,
+  setLoading,
 }) {
   const pages = getArrayFromNumber(noOfPages);
   return (
     <div className={`flex justify-center gap-2 my-10`}>
       {pages.map((page, index) => (
         <div
-          onClick={() => handleChangeCurrentPage(page)}
+          onClick={() => {
+            setLoading(true);
+            handleChangeCurrentPage(page);
+            setTimeout(() => {
+              setLoading(false);
+            }, 2000);
+          }}
           key={index}
           className={`w-8 h-w-8 border-2 flex justify-center items-center duration-300 cursor-pointer ${
             currentPage === page && "bg-slate-300"
